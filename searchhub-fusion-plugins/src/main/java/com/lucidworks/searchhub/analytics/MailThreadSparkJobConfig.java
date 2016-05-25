1b84083d155a407578acfe58bd06ffb01623884f
Jake Mannix <jake.mannix@gmail.com>
2016-05-25 14:50:43 -0700

    Required JSON annotations for discovery

--- searchhub-fusion-plugins/src/main/java/com/lucidworks/searchhub/analytics/MailThreadSparkJobConfig.java
+++ searchhub-fusion-plugins/src/main/java/com/lucidworks/searchhub/analytics/MailThreadSparkJobConfig.java
@@ -1,11 +1,16 @@
 package com.lucidworks.searchhub.analytics;
 
 import com.lucidworks.apollo.spark.SparkJobConfig;
+import org.codehaus.jackson.annotate.JsonCreator;
 import org.codehaus.jackson.annotate.JsonProperty;
+import org.codehaus.jackson.annotate.JsonTypeName;
 
+
+@JsonTypeName("mail-threading")
 public class MailThreadSparkJobConfig extends SparkJobConfig {
   public static final String TYPE = "mail_threading";
 
+  @JsonCreator
   public MailThreadSparkJobConfig(@JsonProperty("id") String id) {
     super(id, TYPE);
   }

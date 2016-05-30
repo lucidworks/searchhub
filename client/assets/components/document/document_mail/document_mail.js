a83944740a58648e0acfc61fb2a0af6970b6621d
Xiaoye Huang <xiaoyehuang@Xiaoyes-MacBook-Pro.local>
2016-05-30 00:30:48 -0700

    delete unnecessary lines and generally find the piece with most highlight word. need to refine regex repression for split and add highlight

--- client/assets/components/document/document_mail/document_mail.js
+++ client/assets/components/document/document_mail/document_mail.js
@@ -23,13 +23,17 @@
 
   }
 
-  function Controller($sce, SnowplowService, $filter, $log) {
+  function Controller($sce, SnowplowService, $filter, $log, QueryService) {
     'ngInject';
     var vm = this;
 
     activate();
 
     function activate() {
+      vm.queryObject = QueryService.getQueryObject();
+      $log.info("see Query Obj in document:");
+      $log.info(vm.queryObject);
+
       vm.postSignal = SnowplowService.postSignal;
       vm.postClickSignal = SnowplowService.postClickSignal;
       vm.doc = processDocument(vm.doc);
@@ -38,10 +42,15 @@
     function processDocument(doc) {
       //make sure we can display the info
       $log.info(doc['subject']);
-      doc['body'] = $sce.trustAsHtml(doc['body']);
+      //doc['body'] = $sce.trustAsHtml(doc['body']);
       doc['subject'] = $sce.trustAsHtml(doc['subject']);
       $log.info(doc['subject']);
       doc['id'] = $sce.trustAsHtml(doc['id']);
+      doc['test']=testFunction(doc['body'],vm.queryObject,100,1);
+      $log.info("see output");
+      $log.info(doc['test']);
+      $log.info("see original");
+      $log.info(doc['body']);
       doc.length_lFormatted = $filter('humanizeFilesize')(doc.length_l);
       doc.lastModified_dtFormatted = $filter('date')(doc.lastModified_dt);
       $log.info("see last modified");
@@ -51,6 +60,37 @@
       return doc;
     }
 
+    function testFunction(para,q,len,skip){
+      //$log.info(para);
+      q=q['q'];
+      //$log.info(q);
+      $log.info(para);
+      $log.info(typeof para);
+      var splittedIntoArray=para.split(/[\s,]+/);
+      $log.info("see splitted");
+      $log.info(splittedIntoArray);
+      var containHighlight=splittedIntoArray.map(a=>a.toLowerCase().includes(q.toLowerCase()));
+      $log.info("check variable");
+      $log.info(containHighlight);
+      $log.info(containHighlight.length);
+      var paraLength=containHighlight.length;
+      //$log.info(paraLength);
+      var max=0;
+      var maxind=0;
+      var i=0;
+      while (i+len<=paraLength){
+        //$log.info("checkpoint");
+        var tmp = containHighlight.slice(i,i+len).reduce((a, b) => a + b, 0);
+        $log.info(tmp);
+        if(tmp>max){
+          max=tmp;
+          maxind=i;
+        }
+        i=i+skip;
+      }
+      return splittedIntoArray.slice(maxind,maxind+len).join(" ");
+
+    }
 
 
   }

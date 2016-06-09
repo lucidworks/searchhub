f354f50c530d5b33c27009aeed91b2ae245e37aa
gsingers <gsingers@apache.org>
2016-06-09 06:13:25 -0400

    utility for docker

---
+++ docker.sh
@@ -0,0 +1,9 @@
+#Simple script for creating and running the docker instance on production
+sudo service docker-searchhub stop
+docker rmi --force searchhub
+docker rm --force searchhub
+gulp build
+cd python
+docker build -t searchhub .
+docker create -p 80:80 --name searchhub searchhub
+sudo service docker-searchhub start

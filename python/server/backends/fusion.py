ab8edb4aa0a1158d7a44cd2db52099d03fcd8e54
gsingers <gsingers@apache.org>
2016-06-25 13:09:22 -0400

    slightly improved expanded display, lower the log level

--- python/server/backends/fusion.py
+++ python/server/backends/fusion.py
@@ -76,7 +76,7 @@ class FusionBackend(Backend):
 
   def set_log_level(self, log_level="WARN"):
     print "Setting Log Level to {0}".format(log_level)
-    resp = self.admin_session.put("apollo/configurations/com.lucidworks.apollo.log.level", data=json.dumps(log_level))
+    resp = self.admin_session.post("apollo/configurations/com.lucidworks.apollo.log.level", data=json.dumps(log_level))
     if resp.status_code != 204:
       print "Unable to set log_level collection to {0}".format(log_level)
       print resp

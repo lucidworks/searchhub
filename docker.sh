5bf3fb95a348cfda75af388698d57a091f09c4dc
gsingers <gsingers@apache.org>
2016-10-21 12:39:50 -0400

    make sure to update the NPM/Bower deps

--- docker.sh
+++ docker.sh
@@ -2,8 +2,11 @@
 sudo service docker-searchhub stop
 docker rmi --force searchhub
 docker rm --force searchhub
+./gradlew installBower
+echo "Building the UI"
 node_modules/gulp/bin/gulp.js build --production
 cd python
+echo "Building Docker Container"
 docker build -t searchhub .
 docker create -p 80:80 --name searchhub searchhub
 sudo service docker-searchhub start

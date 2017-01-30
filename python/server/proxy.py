f2ec6b28b624b8d93ee3806ac5ee5271781733f4
Grant Ingersoll <gsingers@apache.org>
2017-01-30 06:21:19 -0500

    add in failover, fallback (#124)

--- python/server/proxy.py
+++ python/server/proxy.py
@@ -40,8 +40,7 @@ def parse_host_port(h):
 @app.route('/api/<path:other>', methods=["GET", "POST", "PUT"])
 def proxy_request(other):
     start = time.time()
-    fusion_urls = app.config.get("FUSION_URLS", ["http://localhost:8764/api"])
-    fusion_url = fusion_urls[random.randint(0, len(fusion_urls) -1)]
+
     user = app.config.get("FUSION_APP_USER")
     password = app.config.get("FUSION_APP_PASSWORD")
 
@@ -91,16 +90,9 @@ def proxy_request(other):
 
     else:
         form_data = None
-    r = None
+
     #print path
-    if request.method == "POST":
-        r = requests.post("{0}{1}".format(fusion_url, path), data=form_data, headers=request_headers)
-    elif request.method == "PUT":
-        print form_data
-        #print request_headers
-        r = requests.put("{0}{1}".format(fusion_url, path), data=form_data, headers=request_headers)
-    else:
-        r = requests.get("{0}{1}".format(fusion_url, path), headers=request_headers)
+    r = make_request(form_data, app.config.get("FUSION_URLS", ["http://localhost:8764/api"]), path, request_headers)
     the_content_type = r.headers['content-type']
     #print "CT: " + the_content_type
     end = time.time()
@@ -111,5 +103,37 @@ def proxy_request(other):
     return flask_response
 
 
+def make_request(form_data, orig_urls, path, request_headers, timeout=4):
+    fusion_urls = list(orig_urls)# make a copy, since this is destructive
+    r = None
+    success = False
+    i = 0
+    list_len = len(fusion_urls) #calc outside of the loop, as we are going to be removing values from the loop as we go
+    while (i < list_len):
+        #Note: the requests library has built in retry of the underlying connection, so this loop of retries is about trying alternate URLs
+        node_choice = random.randint(0, len(fusion_urls) - 1)
+        fusion_url = fusion_urls[node_choice]
+        if fusion_url is not None:
+            del fusion_urls[node_choice] # Remove it from the list b/c if it fails, we don't want to try again and if we succeed, it doesn't matter
+            #make the request
+            try:
+                if request.method == "POST":
+                    r = requests.post("{0}{1}".format(fusion_url, path), data=form_data, headers=request_headers, timeout=timeout)
+                elif request.method == "PUT":
+                    # print request_headers
+                    r = requests.put("{0}{1}".format(fusion_url, path), data=form_data, headers=request_headers, timeout=timeout)
+                elif request.method == "GET":
+                    r = requests.get("{0}{1}".format(fusion_url, path), headers=request_headers, timeout=timeout)
+                else:
+                    raise Exception("Unsupported request type.  Only POST, PUT and GET are supported through this proxy.")
+            except:
+                print "Request to {0} failed, trying a different URL from: {1} (if empty, we give up!)".format(fusion_url, fusion_urls)
+        else:
+            print "No more URLs to try, giving up for: {0} w/ data: {1}".format(path)
+            break
+        i += 1
+
+    print "Response: {0}".format(r)
+    return r
 
 #app.run(debug=DEBUG_FLAG, host='0.0.0.0', port=LISTEN_PORT)
